5def79c468e014dda5fd8430d1bea9a38f507e0f
joao furukawa <joaovictortfurukawa@gmail.com>
2025-02-23 05:36:45 +0000

    Update route.ts

--- app/api/players/[name]/route.ts
+++ app/api/players/[name]/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { connect2DB } from "../../../../config/db";
 
 export async function GET(
